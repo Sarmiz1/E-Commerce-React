758939de2c6e0540f2e3b6534963935a4296df06
Sarmiz <samueljoseph199515@gmail.com>
2026-05-27 10:31:43 -0700

    fix

--- src/Layout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 
-import Navbar from "../Components/NavBar";
 
 export default function DefaultLayout() {
   return (
